81c4d9e12787601dce46330821bdfad4da8706d4
922643 <922643@LTSA000368.cts.com>
2021-08-02 16:09:59 +0300

    branch2 ok

--- handson_Git/src/handson_Git/GitClass.java
+++ handson_Git/src/handson_Git/GitClass.java
@@ -2,4 +2,13 @@ package handson_Git;
 
 public class GitClass {
 int n = 1;
+public void m2 (){
+	System.out.println("m2");
+}
+public void m3 (){
+	System.out.println("m3");
+}
+public void m4(){
+	System.out.println("m4");
+}
 }
